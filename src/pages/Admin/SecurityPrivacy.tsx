3458c34d0905e9516e4e97cd2862b0574c49fbe3
Sunil Das <sunilpoll1999@gmail.com>
2025-09-18 18:45:16 +0600

    Security & Privacy completed

--- src/pages/Admin/SecurityPrivacy.tsx
+++ src/pages/Admin/SecurityPrivacy.tsx
@@ -1,6 +1,18 @@
+import Compliance from "@/components/admin/Compliance/Compliance"
+import DataEncryption from "@/components/admin/Data Encryption/DataEncryption"
+import GlobalRoleManagement from "@/components/admin/security-privacy/GlobalRoleManagement"
+import Session from "@/components/admin/Session/Session"
+
 const SecurityPrivacy = () => {
   return (
-    <div>SecurityPrivacy</div>
+    <div>
+      <GlobalRoleManagement/>
+      <div className="flex gap-[22px] my-8">
+        <DataEncryption/>
+        <Compliance/>
+      </div>
+      <Session/>
+    </div>
   )
 }
 
