43e4e982850effdcd65c8682de8824f78552c391
Amitav <amitavrchy01@gmail.com>
2025-09-20 17:45:56 +0600

    graphs and charts overview done

--- src/pages/Admin/AdminDashboard.tsx
+++ src/pages/Admin/AdminDashboard.tsx
@@ -1,3 +1,9 @@
+import { ActivityLog } from "@/components/admin/charts/activityLog"
+import { AlertCenter } from "@/components/admin/charts/alertCenter"
+import { EarningReportsChart } from "@/components/admin/charts/earningReport"
+import { SupportTicketsChart } from "@/components/admin/charts/supportTicket"
+import { TopClientsChart } from "@/components/admin/charts/topClients"
+import { TrendingIndustriesTable } from "@/components/admin/charts/trendingIndustries"
 import { CustomerInsights } from "@/components/admin/customerInsight/customerInsight"
 import { MetricsCards } from "@/components/admin/metricsCards/metricsCards"
 
@@ -6,6 +12,29 @@ const AdminDashboard = () => {
     <>
       <MetricsCards />
       <CustomerInsights />
+      <div className="grid grid-cols-12 gap-6 mx-auto mt-10">
+        <div className="col-span-5">
+          <TopClientsChart />
+        </div>
+        <div className="col-span-4">
+          <SupportTicketsChart />
+        </div>
+        <div className="col-span-3">
+          <AlertCenter />
+        </div>
+        <div className="col-span-9 -mt-32">
+          <EarningReportsChart />
+        </div>
+        <div className="col-span-3">
+          <ActivityLog />
+        </div>
+        <div className="col-span-9 -mt-112">
+          <TrendingIndustriesTable />
+        </div>
+        
+      
+        
+      </div>
     </>
   )
 }
