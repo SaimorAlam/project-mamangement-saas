f78140ace9de2d4ec3abfe3064a1dcca673d61fe
naim0018 <mdkazinaim0018@gmail.com>
2025-12-10 18:21:08 +0600

    Build Error

--- src/pages/client/ProgramName.tsx
+++ src/pages/client/ProgramName.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function ProgramName() {
   return (
     <div>ProgramName</div>
--- src/pages/viewerPanel/Projects.tsx
+++ src/pages/viewerPanel/Projects.tsx
@@ -1,4 +1,4 @@
-import CostOverview from "@/common/Charts/CostOverView";
+import CostOverview from "@/common/Charts/CostOverview";
 import PhasePlan from "@/common/Charts/PhasePlan";
 import ProgressChart from "@/common/Charts/Progress";
 import ProgressRing from "@/common/Charts/ProgressRing";
