58802f512fcbb61b2e24569894ef0e2ed663fbef
naim0018 <mdkazinaim0018@gmail.com>
2025-12-12 14:31:48 +0600

    first update

--- src/hooks/useGetUser.tsx
+++ src/hooks/useGetUser.tsx
@@ -1,9 +1,10 @@
 import { useGetProfileQuery } from "@/store/Api/UserApi/UserApi";
+import { User } from "@/types/Auth/Auth";
 
 export const useGetUser = () => {
   const { data } = useGetProfileQuery({});
   const user = data?.data;
-  const { name, role, profileImage, email, id, status } = user;
+  const { name, role, profileImage, email, id, status } = user as User;
   return {
     name,
     role,
