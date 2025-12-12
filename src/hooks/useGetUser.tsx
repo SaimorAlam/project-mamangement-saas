cc5c741d8d8e40cbb487f141b9f3dcb095b3a8e0
naim0018 <mdkazinaim0018@gmail.com>
2025-12-12 14:53:20 +0600

    feat: Enhance user profile management by adding new fields, improving data retrieval with loading/error states, and implementing Redux-based logout with toast feedback.

--- src/hooks/useGetUser.tsx
+++ src/hooks/useGetUser.tsx
@@ -1,15 +1,19 @@
 import { useGetProfileQuery } from "@/store/Api/UserApi/UserApi";
+import { User } from "@/types/Auth/Auth";
 
 export const useGetUser = () => {
-  const { data } = useGetProfileQuery({});
-  const user = data?.data;
-  const { name, role, profileImage, email, id, status } = user;
+  const { data, isLoading, isError } = useGetProfileQuery({});
+
+  const user = data?.data as User | undefined;
+
   return {
-    name,
-    role,
-    profileImage,
-    email,
-    id,
-    status,
+    loading: isLoading,
+    error: isError,
+    name: user?.name || "",
+    role: user?.role || "",
+    profileImage: user?.profileImage || "",
+    email: user?.email || "",
+    id: user?.id || "",
+    status: user?.status || "",
   };
 };
