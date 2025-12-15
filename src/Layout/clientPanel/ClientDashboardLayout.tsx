2eb09528c9292915b05f5bd814eab7a3d8286c4c
naim0018 <mdkazinaim0018@gmail.com>
2025-12-15 08:54:15 +0600

    feat: Implement lazy loading for client routes with a new global loader.

--- src/Layout/clientPanel/ClientDashboardLayout.tsx
+++ src/Layout/clientPanel/ClientDashboardLayout.tsx
@@ -1,26 +1,31 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
 import ClientDashboardHeader from "./ClientDashboardHeader";
 import ClientSidebar from "./ClientSidebar";
+import GlobalLoader from "@/common/GlobalLoader";
+
 
 export default function ClientDashboardLayout() {
   return (
-    <>
-      <SidebarProvider
-        style={
-          {
-            "--sidebar-width": "296px",
-          } as React.CSSProperties
-        }
-      >
-        <ClientSidebar />
-        <main className="w-full px-8">
-          <div className="pb-6">
-            <ClientDashboardHeader name="Timo" />
-          </div>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "296px",
+        } as React.CSSProperties
+      }
+    >
+      <ClientSidebar />
+
+      <main className="w-full px-8">
+        <div className="pb-6">
+          <ClientDashboardHeader name="Timo" />
+        </div>
+
+        <Suspense fallback={<GlobalLoader />}>
           <Outlet />
-        </main>
-      </SidebarProvider>
-    </>
+        </Suspense>
+      </main>
+    </SidebarProvider>
   );
 }
