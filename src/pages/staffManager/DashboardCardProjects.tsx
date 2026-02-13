078ce5086f971eaaac97bc7f86ac6887b54a410f
ahsansoftvence-omega <ahsansoftvence@gmail.com>
2026-02-13 15:00:13 +0600

    Fixed functionalities and UI in s.m. panel

--- src/pages/staffManager/DashboardCardProjects.tsx
+++ src/pages/staffManager/DashboardCardProjects.tsx
@@ -6,11 +6,12 @@ import StaffManagerProjectCard, {
 import { useGetAllProjectsQuery } from "@/store/Api/ProjectApi/ProjectApi";
 import { ChevronLeft } from "lucide-react";
 import { useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 // this is dynamic router component
 const DashboardCardProjects = () => {
   const { status } = useParams();
+  const navigate = useNavigate();
   const managerId = useSelector((state: any) => state.auth.user?.userId);
   const { data, isLoading, error } = useGetAllProjectsQuery({
     managerId,
@@ -50,12 +51,13 @@ const DashboardCardProjects = () => {
         <div className="p-10 bg-gray-50 rounded-lg">
           <p className="text-center text-gray-500">No projects found</p>
         </div>
-        <Link
-          to="/staff-manager-panel"
-          className="border p-3 rounded-lg border-gray-300 mt-5 inline-block"
+        <button
+          onClick={() => navigate(-1)}
+          className="mt-5  flex items-center gap-2 text-primary hover:cursor-pointer border border-gray-300 rounded-lg px-4 py-2"
         >
-          Go Back
-        </Link>
+          <ChevronLeft />
+          Go back
+        </button>
       </>
     );
   }
@@ -65,13 +67,13 @@ const DashboardCardProjects = () => {
         <h4 className="mb-3 text-gray-900 text-xl font-semibold">
           All Program & Project
         </h4>
-        <Link
-          to="/staff-manager-panel/projects"
-          className="flex items-center gap-2 text-primary border border-gray-300 rounded-lg px-4 py-2"
+        <button
+          onClick={() => navigate(-1)}
+          className="flex items-center gap-2 text-primary hover:cursor-pointer border border-gray-300 rounded-lg px-4 py-2"
         >
           <ChevronLeft />
           Go back
-        </Link>
+        </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-5">
         {projects?.map((projectData: StaffEmployeeProject) => {
--- src/routes/StaffManagerRoute.tsx
+++ src/routes/StaffManagerRoute.tsx
@@ -3,7 +3,7 @@ import StaffManagerProjects from "@/pages/staffManager/StaffManagerProjects";
 import StaffManagerMarketingStrategy from "@/pages/staffManager/StaffManagerMarketingStrategy";
 import StaffManagerTimosSuperShop from "@/pages/staffManager/StaffManagerTimosSuperShop";
 import StaffManagerProgramName from "@/pages/staffManager/StaffManagerProgramName";
-import StaffManagerProjectBuilder from "@/pages/staffManager/StaffManagerProjectBuilder";
+// import StaffManagerProjectBuilder from "@/pages/staffManager/StaffManagerProjectBuilder";
 import StaffManagerHighwayExpansion from "@/pages/staffManager/StaffManagerHighwayExpansion";
 // import StaffManagerProjectReview from "@/pages/staffManager/StaffManagerProjectReview";
 import StaffManagerHelp from "@/pages/staffManager/StaffManagerHelp";
@@ -28,6 +28,7 @@ import AllProjectsReview from "@/pages/staffManager/AllProjectsReview";
 import StaffManagerProjectDetail from "@/pages/staffManager/StaffManagerProjectDetail";
 import ProgramDetailsPage from "@/pages/staffManager/ProgramDetailsPage";
 import DashboardCardProjects from "@/pages/staffManager/DashboardCardProjects";
+import ClientProjectBuilder from "@/pages/client/ProjectBuilder/ClientProjectBuilder";
 
 export function getStaffManagerRoutes() {
   return [
@@ -56,7 +57,8 @@ export function getStaffManagerRoutes() {
     { path: "program-name", element: <StaffManagerProgramName /> },
     {
       path: "project-builder",
-      element: <StaffManagerProjectBuilder />,
+      element: <ClientProjectBuilder />,
+      // element: <StaffManagerProjectBuilder />,
     },
     {
       path: "highway-expansion",
