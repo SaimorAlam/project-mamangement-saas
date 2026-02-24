9a4b345832146d327bdb0146f7f387c6f5ae40e5
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2026-02-24 16:02:07 +0600

    modified project page in s.m

--- src/pages/staffManager/StaffManagerProjects.tsx
+++ src/pages/staffManager/StaffManagerProjects.tsx
@@ -1,7 +1,24 @@
 import AllProgramProject from "@/components/staffManager/overview/AllProgramProject";
+import ProjectLocationsMap from "@/components/staffManager/Projects/ProjectLocationsMap";
+import ProjectStats from "@/components/staffManager/Projects/ProjectStats";
+import { useGetProgramAllProjectsQuery } from "@/store/Api/staffManagerApi/StaffManagerApi";
+import { useSelector } from "react-redux";
 
 const StaffManagerProjects = () => {
-  return <AllProgramProject showSearch={true} showViewAll={false} title="All Projects" />;
+  const managerId = useSelector((state: any) => state.auth.user?.userId);
+  
+    const { data } = useGetProgramAllProjectsQuery({
+      managerId});
+      console.log("data fetched are: ",data);
+      
+  return (
+    <div className="flex flex-col gap-4 xl:gap-8">
+      <ProjectStats/>
+  <AllProgramProject showSearch={true} showViewAll={false} title="All Projects" />
+  {/* <ProjectLocationsMap allFilteredProjects={sortedProjects} /> */}
+  <ProjectLocationsMap allFilteredProjects={data?.data?.projects?.data || []} />
+    </div>
+  );
 };
 
 export default StaffManagerProjects;
