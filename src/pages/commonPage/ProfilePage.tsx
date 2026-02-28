5fe22aecbbe1077d9569334eba6ed5895574e4c8
naim0018 <mdkazinaim0018@gmail.com>
2026-02-28 14:37:25 +0600

    sidebar updated

--- src/pages/commonPage/ProfilePage.tsx
+++ src/pages/commonPage/ProfilePage.tsx
@@ -20,7 +20,7 @@ export default function ProfilePage() {
 
   if (isLoading) {
     return (
-      <div className="p-6 space-y-6">
+      <div className=" space-y-6">
         <div className="flex items-center space-x-4">
           <Skeleton className="h-20 w-20 rounded-full" />
           <div className="space-y-2">
